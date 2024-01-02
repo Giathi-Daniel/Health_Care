eabe227cadd7cf8424f9c0d05f2d559eef00afe0
DANIEL GIATHI <giathidaniel252@gmail.com>
2024-01-02 14:10:43 +0300

    dentistry

---
+++ script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".team-swiper", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
